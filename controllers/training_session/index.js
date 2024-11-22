b30b7ec400aaae6c41e252f937fba418abcf39f5
jopszs <stanleycraft00@gmail.com>
2024-11-22 12:44:18 -0300

    UPP

--- controllers/training_session/index.js
+++ controllers/training_session/index.js
@@ -1,4 +1,5 @@
 import {get_training_session} from "./get-training-session.js";
 import {create_training_session} from "./create-training-session.js";
+import {get_training_session_by_user_id} from "./get-all-user-training.js";
 
-export { get_training_session, create_training_session };
+export { get_training_session, create_training_session, get_training_session_by_user_id};
--- routes/training_session.js
+++ routes/training_session.js
@@ -2,9 +2,11 @@ import express from "express";
 import {
     get_training_session,
     create_training_session,
+    get_training_session_by_user_id
 } from "../controllers/training_session/index.js";
 
 export const trainingSessionRoute = express
   .Router()
   .get("/get-training-session", get_training_session)
-  .post("/create-training-session", create_training_session);
+  .post("/create-training-session", create_training_session)
+  .get("/get-all-user-session", get_training_session_by_user_id);
