dcc169388904ef8f9de31e54c06b4cc943ba1b4a
jopszs <stanleycraft00@gmail.com>
2024-12-09 20:31:21 -0300

    upp

--- controllers/user/disable_user.js
+++ controllers/user/disable_user.js
@@ -1,4 +1,5 @@
 import { User } from "../../Banco_de_dados/models/index.js";
+import { User_Training_Session } from "../../Banco_de_dados/models/index.js";
 
 export async function disable_user(req, res) {
     try {
@@ -19,6 +20,33 @@ export async function disable_user(req, res) {
             res.status(404).send({
                 message: 'Usuario não encontrado'
             })
+        }
+
+        const session_running = await User_Training_Session.findOne({
+            where:{
+                UserId: userId,
+                completed: false
+            }
+        })
+        
+        if(!session_running){
+            res.status(404).send({message:"Usuario não possui uma sessão"})
+        }
+        else {
+            const now = new Date();
+
+            await User_Training_Session.update(
+                {
+                    endTime: now,
+                    completed: true
+                },
+                {
+                    where: {
+                        UserId: userId,
+                        completed: false
+                    }
+                },
+            )
         }
 
         res.status(200).send({ message: 'Usuário desativado' })
